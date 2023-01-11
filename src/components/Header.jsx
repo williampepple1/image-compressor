7b655c5a5ecf89628335c8b5d014e9e7c6b97557
William Pepple <williampepple1@gmail.com>
2023-01-11 21:55:30 +0100

    Updated footer and header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import pineapple from '../assets/pineapple.jpg'
 
 const navigation = [
-    { name: 'Solutions', href: '#' },
-    { name: 'Pricing', href: '#' },
-    { name: 'Docs', href: '#' },
-    { name: 'Company', href: '#' },
+    { name: 'Compress your images', href: '#' },
+    { name: 'Jpeg and PNG', href: '#' },
+   
   ]
   
   export default function Header() {
@@ -14,8 +14,8 @@ const navigation = [
           <div className="flex w-full items-center justify-between border-b border-gray-500 py-6 lg:border-none">
             <div className="flex items-center">
               <a href="#">
-                <span className="sr-only">Your Company</span>
-                <img className="h-10 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=white" alt="" />
+                <span className="sr-only">Open Source</span>
+                <img className="h-10 w-auto" src={pineapple} alt="" />
               </a>
               <div className="ml-10 hidden space-x-8 lg:block">
                 {navigation.map((link) => (
@@ -30,13 +30,13 @@ const navigation = [
                 href="#"
                 className="inline-block rounded-md border border-transparent bg-gray-500 py-2 px-4 text-base font-medium text-white hover:bg-opacity-75"
               >
-                Sign in
+                Image Compressor
               </a>
               <a
                 href="#"
                 className="inline-block rounded-md border border-transparent bg-white py-2 px-4 text-base font-medium text-gray-600 hover:bg-gray-50"
               >
-                Sign up
+                Jpeg and PNG files
               </a>
             </div>
           </div>
