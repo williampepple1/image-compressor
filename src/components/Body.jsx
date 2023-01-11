ebe665e02b0d84611c34f0ac5d5cb57eec409a80
William Pepple <williampepple1@gmail.com>
2023-01-11 22:17:23 +0100

    Updated homepage

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import imageCompression from 'browser-image-compression';
 
 const Body = () => {
   return (
-    <div>Body</div>
+    <div className="flex justify-center flex-col space-y-24 items-center h-screen text-4xl font-extrabold text-slate-700">
+    <h1>Compress Jpeg/PNG pictures</h1>      
+    <input type="file" accept="image/*" onchange="handleImageUpload(event)" />
+    </div>
   )
 }
 
